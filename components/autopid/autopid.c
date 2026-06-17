7b448f7b168359a33287650badb4e7cd1112115c
Drew <19173176+amwalters@users.noreply.github.com>
2026-06-18 00:33:11 +1000

    Revert "Cache 12v Supply PID for polling pause eval"
    This reverts commit 9c7760d2151a94db5a337b0e78e9d892033afc90.

--- components/autopid/autopid.c
+++ components/autopid/autopid.c
@@ -815,12 +815,8 @@ static void autopid_data_update(autopid_config_t *pids)
                             parameter_t *param = &curr_pid->parameters[j];
                             if (!param->enabled) continue;
 
-                            // ---> SKIP NON-DEFAULT DESTINATIONS unless 12v supply PID<---
-                            if (param->destination_type != DEST_DEFAULT &&
-                                !(pids->supply_mode_enabled &&
-                                  pids->supply_mode_pid_name &&
-                                  param->name &&
-                                  strcmp(param->name, pids->supply_mode_pid_name) == 0)) continue;
+                            // ---> SKIP NON-DEFAULT DESTINATIONS <---
+                            if (param->destination_type != DEST_DEFAULT) continue;
 
                             if (param->name) {
                                 if (param->raw_string_value != NULL) {
@@ -865,11 +861,7 @@ static void autopid_data_update(autopid_config_t *pids)
                         if (!param->enabled) continue;
 
                         // ---> SKIP NON-DEFAULT DESTINATIONS <---
-                        if (param->destination_type != DEST_DEFAULT &&
-                            !(pids->supply_mode_enabled &&
-                              pids->supply_mode_pid_name &&
-                              param->name &&
-                              strcmp(param->name, pids->supply_mode_pid_name) == 0)) continue;
+                        if (param->destination_type != DEST_DEFAULT) continue;
 
                         if (param->name) {
                             if (param->raw_string_value != NULL) {
@@ -896,11 +888,7 @@ static void autopid_data_update(autopid_config_t *pids)
                     if (!param->enabled) continue;
 
                     // ---> SKIP NON-DEFAULT DESTINATIONS <---
-                    if (param->destination_type != DEST_DEFAULT &&
-                        !(pids->supply_mode_enabled &&
-                          pids->supply_mode_pid_name &&
-                          param->name &&
-                          strcmp(param->name, pids->supply_mode_pid_name) == 0)) continue;
+                    if (param->destination_type != DEST_DEFAULT) continue;
 
                     if (param->name) {
                         if (param->raw_string_value != NULL) {
